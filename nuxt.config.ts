8c85c1f0228b8d79b339482b77e8c0717281de9a
Rafif Hasabi <rafifhasabi@gmail.com>
2025-10-28 20:33:08 +0700

    fix: tailwind config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,32 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-    compatibilityDate: "2025-07-15",
-    devtools: { enabled: true },
-    runtimeConfig: {
-        bpsApiKey: process.env.BPS_API_KEY,
-        public: {},
-    },
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  runtimeConfig: {
+    bpsApiKey: process.env.BPS_API_KEY,
+    public: {},
+  },
   css: ["./app/assets/css/main.css"],
   modules: ["@nuxt/ui"],
 
   vite: {
     plugins: [tailwindcss()],
+  },
+  ui: {
+    colorMode: false,
+    theme: {
+      colors: [
+        "primary",
+        "primary-foreground",
+        "secondary",
+        "tertiary",
+        "info",
+        "success",
+        "warning",
+        "error",
+        "light",
+      ],
+    },
   },
 });
